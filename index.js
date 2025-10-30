b2e334f437205bf4398ae318ef9bf032cc3f5432
Victor Nani <victor.nani1996@gmail.com>
2025-10-30 13:05:37 +0200

    Styling and logic
    Was performed:
     - styling of the page;
     - logic of render of data from array of object
     - logic of increasing / decreasin of likes.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const posts = [
     post: "images/post-vangogh.jpg",
     comment: "just took a few mushrooms lol",
     likes: 21,
+    isLiked: false,
   },
   {
     name: "Gustave Courbet",
@@ -16,6 +17,7 @@ const posts = [
     post: "images/post-courbet.jpg",
     comment: "i'm feelin a bit stressed tbh",
     likes: 4,
+    isLiked: false,
   },
   {
     name: "Joseph Ducreux",
@@ -26,5 +28,70 @@ const posts = [
     comment:
       "gm friends! which coin are YOU stacking up today?? post below and WAGMI!",
     likes: 152,
+    isLiked: false,
   },
 ];
+const mainEl = document.querySelector(".main");
+
+// Liking logic (icon, increase, decrease)
+mainEl.addEventListener("click", function (e) {
+  if (!e.target.dataset.username) return;
+
+  let pressedPost = e.target.dataset.username;
+
+  posts.forEach((post) => {
+    if (post.username === pressedPost) {
+      post.isLiked ? post.likes-- : post.likes++;
+      post.isLiked = !post.isLiked;
+    }
+  });
+
+  renderContent();
+});
+
+// Render of the content and append to the HTML
+function renderContent() {
+  let postContnet = "";
+
+  posts.forEach((post) => {
+    postContnet += `
+        <section class="post">
+          <header class="post-header">
+            <img
+              src="${post.avatar}"
+              alt="Avatar of the user"
+              class="post-avatar"
+            />
+            <div class="post-owner">
+              <p class="fs-l">${post.name}</p>
+              <p>${post.location}</p>
+            </div>
+          </header>
+
+          <img
+            src="${post.post}"
+            alt="${post.name} paint"
+            class="post-img"
+          />
+
+          <footer class="post-interaction">
+            <div class="post-icons mb-s">
+              <i class="fa-regular fa-heart fa-width-auto ${
+                post.isLiked ? "liked" : ""
+              }" data-userName ="${post.username}"></i>
+              <i class="fa-regular fa-comment fa-width-auto"></i>
+              <i class="fa-regular fa-paper-plane fa-width-auto"></i>
+            </div>
+            <p class="post-likes mb-s" >${post.likes} likes</p>
+            <p class="mb-s">
+              <span class="fs-m">${post.username}</span> ${post.comment}
+            </p>
+          </footer>
+        </section> 
+        `;
+  });
+
+  mainEl.innerHTML = postContnet;
+}
+
+renderContent();
